c703b8abb4ee52d6e6581994a1595dacb9cdc9e3
Punnawich Sootthimoke <punnawich.gj@gmail.com>
2026-04-17 04:19:04 +0700

    GJ

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -18,8 +18,7 @@ export async function getOrdersBySeller(seller_id) {
     SELECT o.*,
            u.name AS customer_name
     FROM orders o
-    JOIN customers c ON o.customer_id = c.customer_id
-    JOIN users u ON c.user_id = u.user_id
+    JOIN users u ON o.customer_id = u.user_id
     WHERE o.seller_id = ${seller_id}
     ORDER BY o.order_date DESC
   `;
@@ -48,8 +47,7 @@ export async function getOrdersByCustomer(customer_id) {
     SELECT o.*,
            u.name AS seller_name
     FROM orders o
-    JOIN sellers s ON o.seller_id = s.seller_id
-    JOIN users u ON s.user_id = u.user_id
+    JOIN users u ON o.seller_id = u.user_id
     WHERE o.customer_id = ${customer_id}
     ORDER BY o.order_date DESC
   `;
