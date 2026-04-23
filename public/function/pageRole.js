61a76a553681512811a37f7f41178880fb103f7e
FANNGUB <fanfan.com>
2026-04-24 05:06:04 +0700

    0.1.9

--- public/function/pageRole.js
+++ public/function/pageRole.js
@@ -1,18 +1,19 @@
 export function pageRole() {
-    const role = localStorage.getItemItem('role')
-    if(role == "customer"){
+    const role = localStorage.getItem('role');
+
+    if (role === "customer") {
         window.location.replace("/customer");
-    }else if(role == "seller"){
+    } else if (role === "seller") {
         window.location.replace("/seller");
-    }else if(role == "admin"){
+    } else if (role === "admin") {
         window.location.replace("/admin");
     }
 }
 
-export function checkRole(page){
-    const role = localStorage.getItemItem('role')
-    if(page !== role){
-        window.location.replace("/wrong");
+export function checkRole(page) {
+    const role = localStorage.getItem('role');
 
+    if (page !== role) {
+        window.location.replace("/wrong");
     }
 }
