245e87c0e59f40cd4280cfb6a49a1a3b50cef90d
FANNGUB <fanfan.com>
2026-04-26 23:28:55 +0700

    0.1.9

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -3,14 +3,22 @@ import * as ctrl from "../controllers/usersController.js";
 
 const router = Router();
 
-router.get("/",       ctrl.getAll);
-router.get("/:id",    ctrl.getById);
-router.post("/",      ctrl.create);
-router.put("/:id",    ctrl.update);
-router.delete("/:id", ctrl.remove);
 router.post("/auth", ctrl.login);
+
 router.put("/toggleRole/:id", ctrl.toggleRole);
 router.patch("/status/:id", ctrl.updateStatus);
-router.get("/me/:id" , ctrl.getRole);
+router.get("/me/:id", ctrl.getRole);
+
+
+router.get("/", ctrl.getAll);
+router.post("/", ctrl.create);
+
+
+
+router.get("/:id", ctrl.getById);
+
+router.put("/:id", ctrl.update);
+router.delete("/:id", ctrl.remove);
+
 
 export default router;
